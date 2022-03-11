90e9eebdfee3d4148feceeff5b9a25d3e01af6eb
OmoItohan <itohanomovbude@gmail.com>
2022-03-11 15:15:13 -0800

    solution for task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 01 to 89 with commas and space
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+       	int i;
+	
+	for (i = 48; i < 56; i++)
+	{
+		int j;
+
+		for (j = i + 1; j < 57; j++)
+		{
+			int k;
+
+			for (k = j + 1; k < 58; k++)
+			{
+
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+					continue;
+				putchar(44);
+				putchar(32);
+			}
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
