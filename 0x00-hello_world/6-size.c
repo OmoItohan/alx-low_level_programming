42b7826edbf81cb065bde6f1479be93ee2a6ee62
OmoItohan <itohanomovbude@gmail.com>
2022-03-10 19:22:17 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- main Entry Point
+ *
+ * Description: Prints "The size of various types based on the computer "
+ *
+ * Return: main Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of an long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of an float: %zu byte(s)\n", sizeof(float));
+	printf("Size of an long long int: %zu byte(s)\n", sizeof(long long int);
+	return (0);
+}
