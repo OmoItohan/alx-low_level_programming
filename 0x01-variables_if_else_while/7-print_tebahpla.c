2911a260cb651a5f818414ea29f29b1c876be6a8
OmoItohan <itohanomovbude@gmail.com>
2022-03-11 14:30:44 -0800

    solution for task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-char c;
-for (c = 'z'; c >= 'a'; c--)
-}
-	putchar(c);
-}
+	char c;
 
-	putchar('\n')
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
 	return (0);
 }
