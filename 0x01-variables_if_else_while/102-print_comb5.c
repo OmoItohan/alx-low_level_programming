bdc83aa89399ae179b46327a442dd995b0980f29
OmoItohan <itohanomovbude@gmail.com>
2022-03-11 15:35:47 -0800

    solution for task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 for success
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 48;
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			k = i;
+			while (k < 58)
+			{
+				if (i == k)
+					l = J + 1;
+				else
+					l = 48;
+				while (l < 58)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+					l++;
+				}
+				k++
+			}
+			j++
+		}
+		i++
+	}
+	putchar('\n');
+	return (0);
+}
+
