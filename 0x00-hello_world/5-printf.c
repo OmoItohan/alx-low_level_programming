7894dd6a8c7e85a06755bb9f85f9e8eda87fca42
OmoItohan <itohanomovbude@gmail.com>
2022-03-10 18:39:09 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main- Entry Point
+ *
+ * Description: Prints "With proper grammar, but the outcome is a piece "
+ *
+ * Return: main Always 0
+ *
+ */
+int main(void)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
