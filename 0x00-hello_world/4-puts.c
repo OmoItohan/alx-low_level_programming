7ce6f0e7d37d837f39df8246c460ee0d490a388a
OmoItohan <itohanomovbude@gmail.com>
2022-03-10 18:19:30 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Returns 0
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
