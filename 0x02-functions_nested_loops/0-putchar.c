b559185820cc5e32b15073417c7c1ea579f77ccc
OmoItohan <itohanomovbude@gmail.com>
2022-03-15 07:49:08 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,20 @@
 
 /**
  * main - Entry point.
- * Description: write a program that prints _putchar
+ * Description: write a program that prints "_putchar" followed by a new line.
  * Return: Always 0 (Success.)
  */
 int main(void)
 {
-	char _putchar[7] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	unsigned int c;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (c = o; c < sizeof(_putchar); c++)
-	{
-	_putchar(_putchar[c]);
-	}
-_putchar('\n');
-return (0);
+	return (0);
 }
