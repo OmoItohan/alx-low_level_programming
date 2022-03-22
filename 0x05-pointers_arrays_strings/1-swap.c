28555cfa05c42ead529df31c860962bfaba86748
OmoItohan <itohanomovbude@gmail.com>
2022-03-22 14:51:14 -0700

    pointer and array task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swap value of two ints.
+ * @a: pointer to one int.
+ * @b: pointer to second int.
+ *
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Arrays and ointers in C language
